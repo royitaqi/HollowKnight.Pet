04658063c936f8ea42ab2962d542323ed2c0fa87
royitaqi <royitaqi@users.noreply.github.com>
2023-01-29 22:10:13 -0800

    refactor: rename HookUtils to GameUtils

--- Pet/Utils/GameUtils.cs
+++ Pet/Utils/GameUtils.cs
@@ -7,15 +7,15 @@ using USceneManager = UnityEngine.SceneManagement.SceneManager;
 
 namespace Pet.Utils;
 
-internal static class HookUtils
+internal static class GameUtils
 {
-    public static void Load()
+    internal static void Load()
     {
         USceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
         ModHooks.HeroUpdateHook += ModHooks_HeroUpdateHook;
     }
 
-    public static void Unload()
+    internal static void Unload()
     {
         USceneManager.activeSceneChanged -= SceneManager_activeSceneChanged;
         ModHooks.HeroUpdateHook -= ModHooks_HeroUpdateHook;
@@ -24,31 +24,35 @@ internal static class HookUtils
     #region system hooks
     private static void SceneManager_activeSceneChanged(Scene from, Scene to)
     {
-        typeof(HookUtils).LogModDebug($"SceneManager_activeSceneChanged(from = {from.name}, to = {to.name})");
+        typeof(GameUtils).LogModDebug($"SceneManager_activeSceneChanged(from = {from.name}, to = {to.name})");
 
         if (from.name == "Menu_Title")
         {
-            typeof(HookUtils).LogMod("Game has been loaded");
+            typeof(GameUtils).LogMod("Game has been loaded");
             _gameLoaded?.Invoke();
         }
+
+        typeof(GameUtils).LogModDebug("Game has been loaded");
+        _sceneChanged?.Invoke(from, to);
+
 
         // This triggers about 5ms earlier than `On.QuitToMenu.Start`.
         if (to.name == "Quit_To_Menu")
         {
-            typeof(HookUtils).LogMod("Game is quiting");
+            typeof(GameUtils).LogMod("Game is quiting");
             _onGameQuit?.Invoke();
         }
     }
 
     private static void ModHooks_HeroUpdateHook()
     {
-        typeof(HookUtils).LogModFine("Hero is being updated");
+        typeof(GameUtils).LogModFine("Hero is being updated");
         _onHeroUpdate?.Invoke();
     }
     #endregion system hooks
 
     #region my hooks
-    public static event Action GameLoaded
+    internal static event Action GameLoaded
     {
         add
         {
@@ -62,7 +66,7 @@ internal static class HookUtils
     }
     private static event Action _gameLoaded;
 
-    public static event Action OnGameQuit
+    internal static event Action OnGameQuit
     {
         add
         {
@@ -76,7 +80,21 @@ internal static class HookUtils
     }
     private static event Action _onGameQuit;
 
-    public static event Action OnHeroUpdate
+    internal static event Action<Scene, Scene> SceneChanged
+    {
+        add
+        {
+            _sceneChanged -= value;
+            _sceneChanged += value;
+        }
+        remove
+        {
+            _sceneChanged -= value;
+        }
+    }
+    private static event Action<Scene, Scene> _sceneChanged;
+
+    internal static event Action OnHeroUpdate
     {
         add
         {
@@ -90,4 +108,6 @@ internal static class HookUtils
     }
     private static event Action _onHeroUpdate;
     #endregion my hooks
+
+    internal static bool IsInGame => GameManager.instance.IsGameplayScene();
 }
