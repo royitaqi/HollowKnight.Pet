f641645c86164f85bdae02e89c2218c28521b0bf
royitaqi <royitaqi@users.noreply.github.com>
2023-01-30 20:46:32 -0800

    feat: unlock toll gate machine in Mines_33 dark room

--- Pet/Modules/Lantern.cs
+++ Pet/Modules/Lantern.cs
@@ -1,5 +1,8 @@
-﻿using Pet.Utils;
-using Satchel;
+﻿using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using Pet.Utils;
+using SFCore.Utils;
+using static HutongGames.PlayMaker.FsmEventTarget;
 
 namespace Pet.Modules;
 
@@ -13,13 +16,33 @@ internal class Lantern
         IsLoaded = true;
 
         // set up as if hero has lantern
-        var fsm = LocateVignetteFsm();
+        var fsm = VignetteFsm;
         fsm.ChangeTransition("Dark Lev Check", "DARK 1", "Lantern");
         fsm.ChangeTransition("Dark Lev Check", "DARK 2", "Lantern");
         fsm.ChangeTransition("Scene Reset", "DARK 1", "Lantern 2");
         fsm.ChangeTransition("Scene Reset", "DARK 2", "Lantern 2");
         fsm.ChangeTransition("Scene Reset 2", "DARK 1", "Lantern 2");
         fsm.ChangeTransition("Scene Reset 2", "DARK 2", "Lantern 2");
+
+        GameUtils.OnFsmStart += GameUtils_OnFsmStart;
+    }
+
+    private void GameUtils_OnFsmStart(PlayMakerFSM fsm)
+    {
+        if (IsLoaded && fsm.FsmName == "Disable if No Lantern")
+        {
+            this.LogMod($"Unlocking toll gate machine in dark room");
+            fsm.GetState("Check").Insert(new FsmUtils.InsertParam
+            {
+                Action = new SendEvent
+                {
+                    eventTarget = Self,
+                    sendEvent = FsmEvent.Finished,
+                },
+                Named = "Pet Unlock Toll Gate Machine",
+                Before = typeof(PlayerDataBoolTest),
+            });
+        }
     }
 
     internal void Unload()
@@ -28,17 +51,16 @@ internal class Lantern
         IsLoaded = false;
 
         // recover setup
-        var fsm = LocateVignetteFsm();
+        var fsm = VignetteFsm;
         fsm.ChangeTransition("Dark Lev Check", "DARK 1", "Dark 1");
         fsm.ChangeTransition("Dark Lev Check", "DARK 2", "Dark 2");
         fsm.ChangeTransition("Scene Reset", "DARK 1", "Dark 1 2");
         fsm.ChangeTransition("Scene Reset", "DARK 2", "Dark 2 2");
         fsm.ChangeTransition("Scene Reset 2", "DARK 1", "Dark 1 2");
         fsm.ChangeTransition("Scene Reset 2", "DARK 2", "Dark 2 2");
+
+        GameUtils.OnFsmStart -= GameUtils_OnFsmStart;
     }
 
-    private PlayMakerFSM LocateVignetteFsm()
-    {
-        return HeroController.instance.gameObject.Find("Vignette").GetComponent<PlayMakerFSM>(fsm => fsm.FsmName == "Darkness Control");
-    }
+    private PlayMakerFSM VignetteFsm => HeroController.instance.gameObject.Find("Vignette").GetComponent<PlayMakerFSM>(fsm => fsm.FsmName == "Darkness Control");
 }
